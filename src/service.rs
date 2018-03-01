5d918a81b76e651274ec4b7e40ec465de1828f73
Denis Kolodin <denis.kolodin@xdev.re>
2018-03-01 09:21:42 +0300

    Implement TxCreate transaction

--- src/service.rs
+++ src/service.rs
@@ -3,7 +3,7 @@ use exonum::blockchain::{Blockchain, Service, Transaction, ApiContext};
 use exonum::node::{TransactionSend, ApiSender};
 use exonum::messages::{RawTransaction, Message};
 use exonum::storage::{Fork, MapIndex, Snapshot};
-use exonum::crypto::Hash;
+use exonum::crypto::{Hash, PublicKey};
 use exonum::encoding;
 use exonum::api::{Api, ApiError};
 use iron::prelude::*;
@@ -13,7 +13,18 @@ use serde::Deserialize;
 use serde_json;
 use protocol::*;
 
+// // // // // // // // // // CONSTANTS // // // // // // // // // //
+
+const INIT_BALANCE: u64 = 1000000;
+
 // // // // // // // // // // PERSISTENT DATA // // // // // // // // // //
+
+encoding_struct! {
+    struct Account {
+        owner: &PublicKey,
+        balance: u64,
+    }
+}
 
 // // // // // // // // // // DATA LAYOUT // // // // // // // // // //
 
@@ -25,18 +36,36 @@ impl<T: AsRef<Snapshot>> ExchangeSchema<T> {
     pub fn new(view: T) -> Self {
         ExchangeSchema { view }
     }
+
+    pub fn accounts(&self) -> MapIndex<&Snapshot, PublicKey, Account> {
+        MapIndex::new("cryptoexchange.accounts", self.view.as_ref())
+    }
+
+    pub fn account(&self, owner: &PublicKey) -> Option<Account> {
+        self.accounts().get(owner)
+    }
+}
+
+impl<'a> ExchangeSchema<&'a mut Fork> {
+    pub fn accounts_mut(&mut self) -> MapIndex<&mut Fork, PublicKey, Account> {
+        MapIndex::new("cryptoexchange.accounts", &mut self.view)
+    }
 }
 
 // // // // // // // // // // CONTRACTS // // // // // // // // // //
 
 impl Transaction for TxCreate {
     fn verify(&self) -> bool {
-        true
+        self.verify_signature(self.owner())
     }
 
     fn execute(&self, view: &mut Fork) {
-        info!("Creating account: {:?}", self.owner());
         let mut schema = ExchangeSchema::new(view);
+        if schema.account(self.owner()).is_none() {
+            let account = Account::new(self.owner(), INIT_BALANCE);
+            println!("Create the account: {:?}", account);
+            schema.accounts_mut().put(self.owner(), account);
+        }
     }
 }
 
