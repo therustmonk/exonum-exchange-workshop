093b6bb677c2e22256e2e9b3cb01958044036d49
Denis Kolodin <denis.kolodin@xdev.re>
2018-03-01 09:54:33 +0300

    Show generated public key

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,7 +2,10 @@ extern crate rand;
 extern crate sha2;
 extern crate ed25519_dalek;
 extern crate stdweb;
+extern crate hex;
 
+use stdweb::web::document;
+use stdweb::web::{INode, IParentNode};
 use rand::Rng;
 use rand::OsRng;
 use sha2::Sha512;
@@ -14,5 +17,11 @@ fn main() {
     let mut cspring: OsRng = OsRng::new().unwrap();
     let keypair: Keypair = Keypair::generate::<Sha512>(&mut cspring);
     println!("Keypair: {:?}", keypair);
+    let body = document().query_selector( "body" ).unwrap().unwrap();
+    let div = document().create_element( "div" ).unwrap();
+    let pkey = hex::encode(keypair.public.to_bytes());
+    let message = format!("Public Key: {}", pkey);
+    div.set_text_content(&message);
+    body.append_child(&div);
     stdweb::event_loop();
 }
