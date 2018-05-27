55cf92a2b3077ce8fc702c26c43b58de780fbaf5
Denis Kolodin <DenisKolodin@gmail.com>
2018-05-27 11:47:45 +0300

    Add dotenv and logging

--- live-session/node/src/main.rs
+++ live-session/node/src/main.rs
@@ -4,5 +4,8 @@ extern crate dotenv;
 extern crate exonum;
 
 fn main() {
+    dotenv::dotenv().ok();
+    exonum::helpers::init_logger().unwrap();
+    info!("RustFest Cryptoexchange");
     println!("Hello, world!");
 }
