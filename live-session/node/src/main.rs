c8d6b66f3653a8d41eeb8bdbc4c3420565985a70
Denis Kolodin <DenisKolodin@gmail.com>
2018-05-27 12:11:26 +0300

    Start the node! :)

--- live-session/node/src/main.rs
+++ live-session/node/src/main.rs
@@ -3,6 +3,11 @@ extern crate log;
 extern crate dotenv;
 extern crate exonum;
 
+use exonum::blockchain::{GenesisConfig, ValidatorKeys};
+use exonum::node::{Node, NodeConfig, NodeApiConfig};
+use exonum::storage::MemoryDB;
+use exonum::node::AllowOrigin;
+
 fn node_config() -> NodeConfig {
     let (consensus_public_key, consensus_secret_key) = exonum::crypto::gen_keypair();
     let (service_public_key, service_secret_key) = exonum::crypto::gen_keypair();
@@ -15,7 +20,29 @@ fn node_config() -> NodeConfig {
             validator_keys,
         ].into_iter()
     );
-
+    let api_address = "0.0.0.0:8080".parse().unwrap();
+    let api_cfg = NodeApiConfig {
+        public_api_address: Some(api_address),
+        allow_origin: Some(AllowOrigin::Any),
+        ..Default::default()
+    };
+    let peer_address = "0.0.0.0:2000".parse().unwrap();
+    NodeConfig {
+        listen_address: peer_address,
+        peers: vec![],
+        service_public_key,
+        service_secret_key,
+        consensus_public_key,
+        consensus_secret_key,
+        genesis,
+        external_address: None,
+        network: Default::default(),
+        whitelist: Default::default(),
+        api: api_cfg,
+        mempool: Default::default(),
+        services_configs: Default::default(),
+        database: Default::default(),
+    }
 }
 
 fn main() {
@@ -27,4 +54,5 @@ fn main() {
         vec![],
         node_config(),
     );
+    node.run().unwrap();
 }
